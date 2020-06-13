490ebb177ff6b959dc4a295c804e4d665de3a0c0
SalehAlameeri310 <alameeri.saleh@gmail.com>
2020-06-13 18:42:29 +0300

    done

---
+++ CW-1-SalehAlameeri310.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+let name = "Saleh Alameeri"
+var hobby1 = "Horse Riding/Equestrian"
+var hobby2 = "Football"
+var height = 183
+var age = 16
+print(age)
+print(height)
+print(name)
+print(hobby1)
+print(hobby2)
