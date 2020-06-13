3e962cfda95829be9928c772f07cb18163ef8ac6
SalehAlameeri310 <alameeri.saleh@gmail.com>
2020-06-13 19:51:07 +0300

    done

--- CW-1-SalehAlameeri310.playground/Contents.swift
+++ CW-1-SalehAlameeri310.playground/Contents.swift
@@ -6,8 +6,27 @@ var hobby1 = "Horse Riding/Equestrian"
 var hobby2 = "Football"
 var height = 183
 var age = 16
-print(age)
-print(height)
-print(name)
-print(hobby1)
-print(hobby2)
+print("My age is \(age),my height is \(height),my name is \(name),my first hobby is \(hobby1), and My second hobby is \(hobby2)")
+
+var friendName = "Fahad AlRashed"
+var friendHobby = "Playstation"
+var friendndHobby2 = "Netflix"
+var friendHeight = 163
+var friendAge = 17
+
+name == friendName
+friendHobby == hobby1
+name != friendName
+friendndHobby2 == hobby2
+height >= friendHeight
+height > friendHeight
+height < friendHeight
+height <= friendHeight
+height == friendHeight
+height != friendHeight
+age == friendAge
+age <= friendAge
+age < friendAge
+age >= friendAge
+age > friendAge
+age != friendAge
